9ebc0cd90d126a283e810e80c9c5d9dc56aef61b
Shaurav Thapa <shauravofficial@gmail.com>
2020-04-16 11:46:29 +0100

    game finalize

--- game.py
+++ game.py
@@ -19,14 +19,14 @@ def intro(item):
 
                          ''')
     print_pause(msg)
-    print_pause("You find yourself standing in an edge of "+ item[3] +", fliied with "+ item[4] +".")
+    print_pause("You find yourself standing in an edge of "+ item[3] +", filled with "+ item[4] +".")
     print_pause("Rumor has it that a "+ item[0] +" is somewhere around here, and has been terrifying the nearby area.")
     print_pause("In front of you is a house.")
     print_pause("In your right hand is a dark cave")
     print_pause("In your hand you have your trusty (but not very effective) "+ item[1] +".")
 
 def clear():
-    print("\n\"*100)     
+    print("\n"*100) 
 
 def generate_theme(item):
     # Generates random monster, weapons, place and a scenario for the game
@@ -39,26 +39,28 @@ def generate_theme(item):
 def house(item):
 
     print_pause("You approach the door of the house.")
-    print_pause("You are about to knock when the door opens and out steps a "+ item[0] + ".")
-    print_pause("Eep. This is the "+item[0]+"'s house!")
-    print_pause("The "+item[0]+" attacks you!")
+    print_pause("You are about to knock when the door opens and out steps a "+ item[0] +".")
+    print_pause("Eep. This is the "+ item[0] +"'s house!")
+    print_pause("The "+ item[0] +" attacks you!")
     if '1' not in item:
-        print_pause("You feel a bit under-prepared for this, what with only having a "+item[1] +".")
+        print_pause("You feel a bit under-prepared for this, what with only having a "+ item[1] +".")
 
-    option = input("Would you like to (1) fight or (2) run away?")
+    option = input("Would you like to (1) fight or (2) run away?\n")
     if option == '1':
         if '1' in item:
-            print_pause("As the "+item[0]+" moves to attack, you unseath your new "+item[2])
-            print_pause("The "+ item[2]+ " shines brightly in your hands as you brace yourself for the attack.")
-            print_pause("But the "+item[0] +" takes one look at your shiny toy and runs away!")
+            print_pause("As the "+ item[0] +" moves to attack, you unseath your new "+ item[2])
+            print_pause("The "+ item[2] +" shines brightly in your hands as you brace yourself for the attack.")
+            print_pause("But the "+ item[0] +" takes one look at your shiny toy and runs away!")
             print_pause("You have rid the town of the gorgon. You are victorious!")
-        elif random.randint(0,10)/2 == 0:
+            game_finish()
+        elif random.randint(1,10)%2 != 0:
             print_pause("You do your best....")
-            print_pause("But your "+item[1]+" is no match for the "+item[0]".")
+            print_pause("But your "+ item[1] +" is no match for the "+ item[0] +".")
             print_pause("You have been defeated!")
+            game_finish()
         else:
             print_pause("You do your best....")
-            print_pause("You manage to injure the "+item[0]"'s right eye with the help of your"+item[1]".")
+            print_pause("You manage to injure the "+ item[0] +"'s right eye with the help of your "+ item[1] +".")
             print_pause("You barely make it back into the field. Luckily, you dont seem to have been followed.")
             choice(item)
 
@@ -76,17 +78,16 @@ def cave(item):
     else:
         # walks into the cave the first time
         print_pause("Your eye catches a glint of metal behind a rock.")
-        print_pause("You have found the magical "+item[2]+"!")
-        print_pause("You discard your silly old "+item[1]+"and take the "+item[2]+"with you.")
+        print_pause("You have found the magical "+ item[2] +"!")
+        print_pause("You discard your silly old "+ item[1] +" and take the "+ item[2] +" with you.")
         item.append('1')
-    print_pause("You walk back to the "+item[3]".")
+    print_pause("You walk back to the "+ item[3] +".")
     choice(item)
 
 def choice(item):
     print_pause("Enter 1 to knock on the door of the house.")
     print_pause("Enter 2 to peer into the cave.")
-    print_pause("What would like to do?")
-    area= input("(please enter 1 or 2.)")
+    area= input("What would like to do?\n(please enter 1 or 2.)\n")
     if area =='1':
         house(item)
     elif area =='2':
@@ -95,7 +96,15 @@ def choice(item):
         # if the user puts in anything except 1 or 2 the function keeps repeating itself
         choice(item)             
 
-def game_finish(item):
+def game_finish():
+    play_again=input("Would you like to play again? (y/n)\n")
+    if play_again.lower() == 'y':
+        play_game()
+    elif play_again.lower() == 'n':
+        print_pause("Thank you for playing!")
+    else:
+        game_finish()
+
 
 
 def play_game():
@@ -107,6 +116,6 @@ def play_game():
     choice(item)
 
 
-
+play_game()    
 
 
